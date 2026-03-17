065703b83ecc194566d5e9d6e9470a1bf0e8104d
Damian Jasek <damiannq1337@gmail.com>
2026-03-17 12:51:44 +0100

    Add initial project structure with CMake configuration and gitignore files

---
+++ engine/include/core/engine.h
@@ -0,0 +1,15 @@
+#ifndef DZeMIKK_ENGINE_H
+#define DZeMIKK_ENGINE_H
+
+namespace engine {
+    class engine {
+    private:
+    public:
+        engine();
+        ~engine();
+    };
+}
+
+
+
+#endif //DZeMIKK_ENGINE_H
---
+++ engine/src/core/engine.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include "../../include/core/engine.h"
+
+#include "third_party/glad/glad.h"
+#include "third_party/GLFW/glfw3.h"
+
+engine::engine::engine() {
+    printf("Hello Engine\n");
+
+    if (!glfwInit()) {
+        std::fprintf(stderr, "Failed to initialize GLFW\n");
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW + GLAD", nullptr, nullptr);
+    if (!window) {
+        std::fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::fprintf(stderr, "Failed to initialize GLAD\n");
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.15f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+engine::engine::~engine() = default;
