cc1c21bab942c011f041a4059c66bbc36cf28793
Damian Jasek <damiannq1337@gmail.com>
2026-03-20 18:08:31 +0100

    Expand CMake configuration: integrate GLM, Freetype, ImGui, and Assimp libraries; add support for associated includes and linking.

--- DZeMIKK/src/core/engine.cpp
+++ DZeMIKK/src/core/engine.cpp
@@ -1,21 +1,27 @@
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include "core/engine.h"
-#include "spdlog/spdlog.h"
-
-#include <cstdio>
+#include <spdlog/spdlog.h>
+#include <assimp/version.h>
+#include <glm/glm/detail/setup.hpp>
 
 dzemikk::Engine::Engine() {
-    spdlog::info("DZeMIKK");
+    spdlog::info("DZeMIKK 1.0.0");
+    spdlog::info("GLM version: {}.{}.{}", GLM_VERSION_MAJOR, GLM_VERSION_MINOR, GLM_VERSION_PATCH);
+    spdlog::info("Assimp version: {}.{}.{}",
+        aiGetVersionMajor(),
+        aiGetVersionMinor(),
+        aiGetVersionRevision());
+
     spdlog::info("spdlog version: {}.{}.{}",
-             SPDLOG_VER_MAJOR,
-             SPDLOG_VER_MINOR,
-             SPDLOG_VER_PATCH);
+        SPDLOG_VER_MAJOR,
+        SPDLOG_VER_MINOR,
+        SPDLOG_VER_PATCH);
 
     if (!glfwInit()) {
         spdlog::critical("Failed to initialize GLFW:");
     }else {
-        spdlog::info("GLFW version: {}", glfwGetVersionString());
+        spdlog::info("GLFW version: {}", GLFW_CONTEXT_VERSION_MAJOR);
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,5 +1,9 @@
 #include <core/engine.h>
-
+#include <extern/imgui/imgui.h>
 int main() {
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
     dzemikk::Engine e;
 }
