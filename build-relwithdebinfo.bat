8e894deeaa95edd65c324285f1ead7f01eb41903
Damian Jasek <damiannq1337@gmail.com>
2026-03-28 22:31:33 +0100

    Refactor build scripts

--- build-relwithdebinfo.bat
+++ build-relwithdebinfo.bat
@@ -1,7 +1,8 @@
 @echo off
 setlocal
+echo %1
 
-cmake -S . -B cmake-build-relwithdebinfo -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake -S . -B cmake-build-relwithdebinfo -DCMAKE_BUILD_TYPE=RelWithDebInfo -DFMOD_LIBS_PATH=%1
 if errorlevel 1 exit /b 1
 
 cmake --build cmake-build-relwithdebinfo --target game -j 10
@@ -9,3 +10,4 @@ if errorlevel 1 exit /b 1
 
 echo RelWithDebInfo build finished successfully.
 endlocal
+pause
