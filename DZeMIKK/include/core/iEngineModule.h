6281312d57e6e6b6f593199deb5cdfd6af0e0ae9
Damian Jasek <damiannq1337@gmail.com>
2026-04-15 11:25:33 +0200

    Minor improvements for IEngineModule

--- DZeMIKK/include/core/iEngineModule.h
+++ DZeMIKK/include/core/iEngineModule.h
@@ -3,7 +3,15 @@
 namespace dzemikk {
     class IEngineModule {
       public:
+        IEngineModule() = default;
         virtual ~IEngineModule() = default;
+
+        IEngineModule(const IEngineModule&) = delete;
+        IEngineModule& operator=(const IEngineModule&) = delete;
+
+        IEngineModule(IEngineModule&&) = delete;
+        IEngineModule& operator=(IEngineModule&&) = delete;
+
         virtual void Initialize() = 0;
         virtual void UnInitialize() = 0;
     };
