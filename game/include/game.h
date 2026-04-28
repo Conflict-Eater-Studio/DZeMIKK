35e6704e825c75e8f4ccc6d7f4d42d8eed8a7d26
Damian Jasek <damiannq1337@gmail.com>
2026-04-28 21:14:52 +0200

    Refactor `Game` class by modularizing setup functions, adding scene initialization, and organizing input callbacks.

--- game/include/game.h
+++ game/include/game.h
@@ -2,20 +2,39 @@
 #define DZEMIKK_GAME_H
 #include "core/engine.h"
 
+namespace dzemikk {
+class Material;
+class GameObject;
+}
 class Game {
 public:
-    Game(dzemikk::Engine* engine) {
-        this->engine = engine;
-        init();
-    }
+    explicit Game(dzemikk::Engine* engine);
     ~Game() = default;
 
-    void init();
     void start();
+private:
+    void setupScene();
+    void setupSkybox();
+    void setupMainCamera();
+    void setupMaterials();
+    void setupWorld();
+    void setupPlayer();
+    void setupChest();
+    void setupEnemy();
+    void setupUICamera();
+    void setupUI();
+    void setupButton(dzemikk::GameObject* canvasGo);
+    void setupSlider(dzemikk::GameObject* canvasGo);
+    void setupCheckbox(dzemikk::GameObject* canvasGo);
+    void setupAudio();
+    void setupInputCallbacks();
 
-  private:
     dzemikk::Engine* engine;
-
+    std::shared_ptr<dzemikk::Scene> mainScene;
+    dzemikk::GameObject* playerGO = nullptr;
+    dzemikk::Material* materialA = nullptr;
+    dzemikk::Material* materialB = nullptr;
+    dzemikk::Material* quadMaterial = nullptr;
 };
 
 #endif
