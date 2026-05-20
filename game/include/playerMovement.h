08a8d5a583540ddff1e9acac250ab4aec559ff8c
Damian Jasek <damiannq1337@gmail.com>
2026-05-20 17:17:43 +0200

    Add `PlayerMovement` component with movement and pathfinding logic

---
+++ game/include/playerMovement.h
@@ -0,0 +1,35 @@
+#ifndef DZEMIKK_PLAYERMOVEMENT_H
+#define DZEMIKK_PLAYERMOVEMENT_H
+
+#include "ecs/components/monobehaviour.h"
+#include "map/HexGrid.h"
+#include "map/PlayerEntity.h"
+
+namespace game {
+    class PlayerMovement : public dzemikk::MonoBehaviour {
+        public:
+        PlayerMovement() = default;
+
+        void start() override;
+        void update(double deltaTime) override;
+        [[nodiscard]] std::string typeName() const override {
+            return "PlayerMovement";
+        }
+
+        void setSpeed(float speed);
+        float getSpeed() const;
+        void setPlayerEntity(PlayerEntity* playerEntity);
+        void setHexGrid(HexGrid* hexGrid);
+        void moveTo(HexGrid::HexCellPtr cell);
+    private:
+        PlayerEntity* _playerEntity = nullptr;
+        HexGrid* _hexGrid = nullptr;
+        std::vector<HexGrid::HexCellPtr> _path;
+        float _speed = 1.0f;
+        int _step = 1;
+        float _duration = 0.0f;
+
+    };
+}
+
+#endif
