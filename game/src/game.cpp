df972d11d9f17793eb63fba5bb1e5e8e5a8d4004
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2026-06-24 16:51:10 +0200

    Camera fix

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -1293,6 +1293,7 @@ void Game::setupPlayer() {
     combatPlayerPanel->setAssetManager(_engine->getAssetManager());
     combatPlayerPanel->setCanvas(playerPanel);
     combatPlayerPanel->setEngine(_engine);
+    combatPlayerPanel->setGame(this);
 
     auto* playerHealthGO = _mainScene.get()
                                ->findGameObjectByName("Player_Avatar_Panel")
